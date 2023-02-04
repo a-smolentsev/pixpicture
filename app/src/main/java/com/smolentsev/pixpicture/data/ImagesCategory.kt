9428ae867a0c8e461c909a381fdf98d9fc28eeba
anton <ant.smolenzev@gmail.com>
2023-02-04 13:26:14 +0300

    Splash Screen

--- app/src/main/java/com/smolentsev/pixpicture/data/ImagesCategory.kt
+++ app/src/main/java/com/smolentsev/pixpicture/data/ImagesCategory.kt
@@ -4,12 +4,9 @@ package com.smolentsev.pixpicture.data
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
+
 data class ImagesCategory(
-    @SerialName("hits")
     val hits: List<Hit>,
-    @SerialName("total")
     val total: Int,
-    @SerialName("totalHits")
     val totalHits: Int
 )
